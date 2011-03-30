2217ec220a163d1023498464711ce52aea4031ea
chrisrhoden <carhoden@gmail.com>
2011-03-30 16:47:44 -0400

    eliminate typos

--- lib/s3_swf_upload/view_helpers.rb
+++ lib/s3_swf_upload/view_helpers.rb
@@ -57,7 +57,7 @@ module S3SwfUpload
                 #{canceled}
               },
               onClear: function(){
-                #{clear}
+                #{cleared}
               },
               onRemove: function(){
                 #{remove}
